e6e104a5c7c2d098d4d7f3a2ae9f69b80aded98f
Timothy Perry <perry2of5@yahoo.com>
2025-04-29 09:56:29 -0700

    added history, which allows asking the bot to try again, but also causes it to not query the database when it needs to. History needs work

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 import asyncio
-
+from pydantic_ai.messages import ModelMessage, ModelRequest, ModelResponse, TextPart, UserPromptPart
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
@@ -28,18 +28,54 @@ dbmcp = MCPServerStdio(
     }
 )
 
-agent = Agent(model=ollama_model, mcp_servers=[dbmcp],
-            instructions="""
+instructions="""
             You are a data analyst for a company selling models of classic cars. Use the classicmodels database
-            to answer questions. Please keep answers short and to the point. If the question cannot be answered
+            to answer questions. Keep answers short and to the point. If the question cannot be answered
             based on the database, say so. Do not guess.
-            """)
+
+            For queries which depend on knowing which month an order is in use the orderDate column in the 
+            orders table.
+
+            For queries asking about the cost of an order or the selling price of a product use the priceEach
+            column of the orderdetails table.
+
+            Understand the terms car, model car, car model, item, and product to refer to records the products table unless it is an
+            item in an order in which case use the orderdetails table to determine the price.
+
+            Use markdown to format answers. 
+
+            All answers must be in solely English. If an answer is initially written in another language,
+            translate the answer to english before displaying and do not display any text from the other
+            language.
+
+            Query the classicmodels database to answer each request. Only use data from the classicmodels
+            database to answer requests.
+            """
+
+agent = Agent(model=ollama_model, mcp_servers=[dbmcp], instructions=instructions)
+
+def gradio_to_ai_hist(history) -> list[ModelMessage]:
+    model_history = []
+    if history is not None:
+        for gr_hist in history:
+            if gr_hist['role'] == 'user':
+                model_history.append(ModelRequest(
+                    parts=[UserPromptPart(gr_hist['content'])],
+                    instructions=instructions
+                ))
+            else:
+                model_history.append(ModelResponse(
+                    parts=[TextPart(content=gr_hist['content'])]
+                ))
+    return model_history
+
 
 async def query_model_classic_car_db(message: str, history) -> str:
     async with agent.run_mcp_servers():
         print("processing: " + message)
-        result = await agent.run(message)
+        result = await agent.run(message, message_history=gradio_to_ai_hist(history))
     return result.output
+
 
 
 import gradio as gr
@@ -50,7 +86,7 @@ demo = gr.ChatInterface(
 )
 
 async def launcher():
-    demo.launch(share=True)
+    demo.launch(share=False)
 
 def main():
     asyncio.run(launcher())
